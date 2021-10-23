da5b5dc35fb9cbf100c365d57b83edcef06357e8
Augustin <sorelaugustin@gmail.com>
2021-10-23 19:31:50 +0100

    server defaultt layout added

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-//
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io");
+
+server.listen(5000, () =>
+  console.log("server running => http://localhost:5000")
+);
